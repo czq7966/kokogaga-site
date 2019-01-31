9177f4f2ac86afc910d7b040620f726fbb8907b0
陈志谦 <761208@nd.com>
2019-01-31 22:30:10 +0800

    增加Peer相关指令
    Change-Id: I60aeb92ddbec249a5dcf3f621e024ad7fd7e4e4c

--- src/server/cmds/common.ts
+++ src/server/cmds/common.ts
@@ -15,20 +15,55 @@ new CommandCommon({instanceId: Dts.dispatcherInstanceName});
 Common.CommandTypes.RegistCommandType({
     cmdId: Dts.ECommandId.adhoc_hello,
     name: '握手',
-    ReqClass: CommandCommon as any,
-    RespClass: CommandCommon as any
+    ReqClass: CommandCommon,
+    RespClass: CommandCommon
 })
 
 Common.CommandTypes.RegistCommandType({
     cmdId: Dts.ECommandId.room_hello,
     name: '握手',
-    ReqClass: CommandCommon as any,
-    RespClass: CommandCommon as any
+    ReqClass: CommandCommon,
+    RespClass: CommandCommon
 })
 
 Common.CommandTypes.RegistCommandType({
     cmdId: Dts.ECommandId.stream_room_hello,
     name: '握手',
-    ReqClass: CommandCommon as any,
-    RespClass: CommandCommon as any
+    ReqClass: CommandCommon,
+    RespClass: CommandCommon
+})
+
+Common.CommandTypes.RegistCommandType({
+    cmdId: Dts.ECommandId.stream_webrtc_offer,
+    name: 'offer',
+    ReqClass: CommandCommon,
+    RespClass: CommandCommon
+})
+
+Common.CommandTypes.RegistCommandType({
+    cmdId: Dts.ECommandId.stream_webrtc_answer,
+    name: 'answer',
+    ReqClass: CommandCommon,
+    RespClass: CommandCommon
+})
+
+Common.CommandTypes.RegistCommandType({
+    cmdId: Dts.ECommandId.stream_webrtc_sdp,
+    name: 'sdp',
+    ReqClass: CommandCommon,
+    RespClass: CommandCommon
+})
+
+Common.CommandTypes.RegistCommandType({
+    cmdId: Dts.ECommandId.stream_webrtc_candidate,
+    name: 'candidate',
+    ReqClass: CommandCommon,
+    RespClass: CommandCommon
+})
+
+Common.CommandTypes.RegistCommandType({
+    cmdId: Dts.ECommandId.stream_webrtc_ready,
+    name: 'ready',
+    ReqClass: CommandCommon,
+    RespClass: CommandCommon
 })
