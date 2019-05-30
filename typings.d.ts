80ffae7209783ce50d186e060825b78813d44be1
陈志谦 <761208@nd.com>
2019-05-30 19:45:59 +0800

    增加全局环境变量IsNode
    Change-Id: I65a45e23ffc681e7ae62325662fb18bd0fb19415

---
+++ typings.d.ts
@@ -0,0 +1,10 @@
+declare var adhoc_cast_connection_console : {
+    log(...args: any[])
+    warn(...args: any[])
+    dir(...args: any[])
+    trace(...args: any[])
+    error(...args: any[])
+}
+
+declare var global: any;
+declare var IsNode: boolean;
