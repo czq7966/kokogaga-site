056b40ce11897872e9fca28899bd3ac5c324d83f
陈志谦 <761208@nd.com>
2019-12-10 15:12:47 +0800

    add sdp-probe interface for sdp k8s
    Change-Id: Idff00d4492feab071047fb9930a636fbb42ad5bb

--- webpack.config.js
+++ webpack.config.js
@@ -16,4 +16,5 @@ else if (process.env.NODE_MODULE === 'mdm-receiver')
     module.exports = require('./webpack/webpack.config.mdm.receiver.js');
 else if (process.env.NODE_MODULE === 'mdm-server') 
     module.exports = require('./webpack/webpack.config.mdm.server.js');
-  
+else if (process.env.NODE_MODULE === 'sdp-probe') 
+    module.exports = require('./webpack/webpack.config.sdp-probe.js');  
